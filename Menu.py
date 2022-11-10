2a13c005ee6720f01d10c4a0a8cdfae8248d9369
Volta41S <angeldtp07@hotmail.com>
2022-11-09 22:42:36 -0600

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -1,5 +1,6 @@
 import tkinter
 from tkinter import *
+from tkinter import ttk
 from PIL import ImageTk, Image
 import os
 
@@ -9,11 +10,18 @@ def Menu_Secundario():
     Menu.geometry("1200x700")
     Menu.title("Herramienta KZAJPJ - Menu principal")
     #Menu.resizable(0,-30)
+    """style = ttk.Style()
+    style.configure("BW.TLabel", foreground="white", background="#8B1C0E") 
+    style = ttk.Style()
+    style.map("C.TButton",
+    foreground=[('!disabled', 'white'), ('active', 'black')],
+    background=[('!disabled', '#8B1C0E'), ('active', '#8B1C0E')], padding=3)"""
 
     Menu.configure(background="white")
     Label(Menu, text="Acceso al sistema", bg="white", fg="black", width="300", height="3", font=("Arial", 15)).pack()
     #Label(text="").pack()
     
+    #btn1=ttk.Button(Menu, style="C.TButton", text="Seleccionar")
     btn1=Button(Menu,bg="#8B1C0E", fg="white", text="Seleccionar", height="2", width="15")
     btn1.place(x=1000, y= 600)
     #Label(text="").pack()
